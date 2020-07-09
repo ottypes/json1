94a14eef0c59eda2c11be28194f2ede8ad35c27b
Seph Gentle <me@josephg.com>
2020-07-09 12:02:33 +1000

    Added typescript exports for the type of operations and their components. Also exported the ConflictType enum

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,7 @@
 export * from './json1.release.js'
 export {ReadCursor, WriteCursor} from './cursor.js'
+export {
+  Doc,
+  JSONOp, JSONOpList, JSONOpComponent,
+  Key, Path, Conflict, ConflictType,
+} from './types'
